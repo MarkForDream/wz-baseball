619afcb01cd5b2cd6a7fc0cf2b93f106406e06e8
pptang <ptang8@bus.illinois.edu>
2016-04-19 21:17:41 +0800

    add sample step-by-step form

--- client/backend/js/routers/router.js
+++ client/backend/js/routers/router.js
@@ -1,4 +1,4 @@
-angular.module('backend.router', ['ui.router', 'backend.main.controller']).config(function($stateProvider, $urlRouterProvider) {
+angular.module('backend.router', ['ngAnimate', 'ui.router', 'backend.main.controller']).config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/backend');
 
     $stateProvider
@@ -23,5 +23,28 @@ angular.module('backend.router', ['ui.router', 'backend.main.controller']).confi
                     templateUrl: '/backend/views/home.html'
                 }
             }
+        })
+        .state('backend.newOrder', {
+            url: '/newOrder',
+            views: {
+                'container@': {
+                    templateUrl: '/backend/views/newOrder.html',
+                    controller: 'OrderController'
+                }
+            }
+        })
+        .state('backend.newOrder.contact', {
+            url: '/contact',
+            templateUrl: '/backend/views/newOrder-contact.html'
+            
+        })
+        .state('backend.newOrder.handers', {
+            url: '/handers',
+            templateUrl: '/backend/views/newOrder-handers.html'
+            
+        })
+        .state('backend.newOrder.payment', {
+            url: '/payment',
+            templateUrl: '/backend/views/newOrder-payment.html'
         });
 });
